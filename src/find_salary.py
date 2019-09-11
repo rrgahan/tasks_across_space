8198052262b40c226673f3226d3713243ce1a4fd
Reid Gahan <rgahan@dontpaniclabs.com>
2019-09-10 21:16:25 -0500

    Find salary changes

--- src/find_salary.py
+++ src/find_salary.py
@@ -1,11 +1,13 @@
 import csv
 import itertools
 import nltk
+import numbers
 import pandas as pd
+import re
 
 
 def main():
-    data = pd.read_csv('data/job_postings_large.csv')
+    data = pd.read_csv('data/job_postings_large.csv', encoding="latin")
     descriptions = data.description
 
     tokenizer = nltk.tokenize.RegexpTokenizer('\w+|\$[\d\.]+|\S+')
@@ -20,43 +22,65 @@ def main():
     flag_df.starting_bonus = find_starting_bonus(descriptions)
     flag_df.retirement_plans = find_retirement_plans(descriptions)
     flag_df.insurance = find_insurance(descriptions, tokenizer)
-    flag_df.to_csv("output/salary_subset.csv", index=False)
+    flag_df.to_csv("output/salary_all_v3.csv", index=False)
     print(flag_df)
 
 
 def find_wage(descriptions, tokenizer):
-    pre_keywords = ["earn", "pay", "get", "salary", "earned", "paid", "got", "rate", "rates"]
-    wage_frequency_keywords = ["weekend", "weekly", "week", "hourly", "hour", "yearly", "year", "salary", "annual", "annually"]
+    pre_keywords = ["earn", "earns", "earning", "earned",
+                    "pay", "pays", "paying", "paid",
+                    "get", "gets", "getting", "got"
+                    "salary", "rate", "rates", "wage", "wages",
+                    "make", "makes", "making", "made"]
+    wage_frequency_keywords = ["weekly", "week", "wk",
+                               "hourly", "hour", "hr",
+                               "yearly", "year", "yr",
+                               "annual", "annually", 
+                               "weekend", "salary",  "mile"]
 
     wage_amount = []
     wage_frequency = []
 
     for description in descriptions:
-        tokens = tokenizer.tokenize(description)
-        tokens_length = len(tokens) - 1
-        description_wage_amount = []
-        description_wage_frequency = ""
-        for i, token in enumerate(tokens):
-            if token in pre_keywords:
-                # Look for "$", then amount, then frequency
-                dollar_index = 0
-                dollar_ending_index = len(tokens) if i + 10 >= len(tokens) else i + 10
-                for i in range(i, dollar_ending_index):
-                    if "$" in tokens[i]:
-                        dollar_index = i
-                        wage_index, amount = find_amount(dollar_index + 1, tokens)
-                        description_wage_amount.append(amount)
-                        if wage_index:
-                            frequency = find_frequency(wage_index, tokens, wage_frequency_keywords)
-                            if frequency:
-                                description_wage_frequency = frequency
+        try:
+            tokens = tokenizer.tokenize(description)
+            tokens_length = len(tokens) - 1
+            description_wage_amount = []
+            description_wage_frequency = []
+            for i, token in enumerate(tokens):
+                if token.lower() == "hourly rate":
+                    dollar_index = 0
+                    dollar_ending_index = len(tokens) if i + 10 >= len(tokens) else i + 10
+                    for i in range(i, dollar_ending_index):
+                        if "$" in tokens[i]:
+                            dollar_index = i
+                            wage_index, amount = find_amount(dollar_index + 1, tokens)
+                            description_wage_amount.append(amount)
+                            if wage_index:
+                                description_wage_frequency.append("hourly")
+                if token.lower() in pre_keywords:
+                    # Look for "$", then amount, then frequency
+                    dollar_index = 0
+                    dollar_ending_index = len(tokens) if i + 10 >= len(tokens) else i + 10
+                    for i in range(i, dollar_ending_index):
+                        if "$" in tokens[i]:
+                            dollar_index = i
+                            wage_index, amount = find_amount(dollar_index + 1, tokens)
+                            description_wage_amount.append(amount)
+                            if wage_index:
+                                frequency = find_frequency(wage_index, tokens, wage_frequency_keywords)
+                                if frequency:
+                                    description_wage_frequency.append(frequency)
 
-            if description_wage_amount and description_wage_frequency:
-                break
-        if description_wage_frequency:
-            wage_amount.append(description_wage_amount)
-            wage_frequency.append(description_wage_frequency)
-        else:
+                if description_wage_amount and description_wage_frequency:
+                    break
+            if description_wage_frequency:
+                wage_amount.append(description_wage_amount)
+                wage_frequency.append(description_wage_frequency)
+            else:
+                wage_amount.append([])
+                wage_frequency.append("")
+        except:
             wage_amount.append([])
             wage_frequency.append("")
 
@@ -68,31 +92,29 @@ def find_amount(starting_index, tokens):
     ending_index = len(tokens) if starting_index + 6 >= len(tokens) else starting_index + 6
     wage_index = 0
     wage = ""
-    numbers = []
+    amounts = []
     # Find near numbers
     for i in range(starting_index, ending_index):
         if is_number(tokens[i]):
-            numbers.append(tokens[i])
+            amounts.append(tokens[i])
             wage_index = i
-        elif tokens[i].lower() == "k":
-            numbers.append("000")
         elif "$" in tokens[i]:
             break
 
-    if len(numbers) == 1:
-        wage = numbers[0]
-    elif len(numbers) == 2:
+    if len(amounts) == 1:
+        wage = amounts[0]
+    elif len(amounts) == 2:
         # This would be cents
-        if len(numbers[1]) == 2:
-            wage = "{}.{}".format(numbers[0], numbers[1])
+        if len(amounts[1]) == 2:
+            wage = "{}.{}".format(amounts[0], amounts[1])
         # This would be thousands
-        if len(numbers[1]) == 3:
-            wage = "{},{}".format(numbers[0], numbers[1])
+        if len(amounts[1]) == 3:
+            wage = "{},{}".format(amounts[0], amounts[1])
     return (wage_index, wage)
 
 
 def find_frequency(starting_index, tokens, wage_frequency_keywords):
-    ending_index = len(tokens) if starting_index + 8 >= len(tokens) else starting_index + 8
+    ending_index = len(tokens) if starting_index + 3 >= len(tokens) else starting_index + 3
     for i in range(starting_index, ending_index):
         if tokens[i] in wage_frequency_keywords:
             return tokens[i]
@@ -104,12 +126,15 @@ def find_starting_bonus(descriptions):
     flags = []
 
     for description in descriptions:
-        flag = False
-        for keyword in bonus_keywords:
-            if keyword in description:
-                flag = True
-                break
-        flags.append(flag)
+        try:
+            flag = False
+            for keyword in bonus_keywords:
+                if keyword in description:
+                    flag = True
+                    break
+            flags.append(flag)
+        except:
+            flags.append(False)
 
     return flags
 
@@ -119,12 +144,15 @@ def find_retirement_plans(descriptions):
     flags = []
 
     for description in descriptions:
-        flag = False
-        for keyword in retirement_keywords:
-            if keyword in description:
-                flag = True
-                break
-        flags.append(flag)
+        try:
+            flag = False
+            for keyword in retirement_keywords:
+                if keyword in description:
+                    flag = True
+                    break
+            flags.append(flag)
+        except:
+            flags.append(False)
 
     return flags
 
@@ -136,35 +164,37 @@ def find_insurance(descriptions, tokenizer):
 
     flags = []
 
-    for description in descriptions:
-        flag = False
-        tokens = tokenizer.tokenize(description)
-        for i, token in enumerate(tokens):
-            if token in insurance_keywords_first:
-                next_index = i + 1
-                if tokens[next_index] in insurance_keywords_second:
+    for i, description in enumerate(descriptions):
+        try:
+            flag = False
+            tokens = tokenizer.tokenize(description)
+            for i, token in enumerate(tokens):
+                if token in insurance_keywords_first:
+                    next_index = len(tokens) if i + 1 >= len(tokens) else i + 1
+                    if tokens[next_index] in insurance_keywords_second:
+                        flag = True
+                        break
+                    else:
+                        max_search_index = len(tokens) if next_index + 4 >= len(tokens) else next_index + 4
+                        for j in range(next_index, max_search_index):
+                            if tokens[j] in insurance_keywords_first:
+                                flag = True
+                                break
+                if token in insurance_keywords_combined:
                     flag = True
                     break
-                else:
-                    max_search_index = len(tokens) if next_index + 4 >= len(tokens) else next_index + 4
-                    for j in range(next_index, max_search_index):
-                        if tokens[j] in insurance_keywords_first:
-                            flag = True
-                            break
-            if token in insurance_keywords_combined:
-                flag = True
-                break
-        flags.append(flag)
+            flags.append(flag)
+        except:
+            flags.append(False)
 
     return flags
 
 
 def is_number(s):
-    try:
-        float(s)
+    pattern = re.compile("(\d+ *[kK]*)")
+    if pattern.search(s):
         return True
-    except ValueError:
-        return False
+    return False
 
 
 if __name__ == "__main__":
